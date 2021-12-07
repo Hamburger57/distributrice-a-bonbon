c93410c8104845afc7823c0789eff59746265766
Hamburger57 <93205979+Hamburger57@users.noreply.github.com>
2021-12-07 11:50:26 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,5 +5,6 @@ basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P16) == 1) {
         servos.P0.setAngle(60)
         basic.pause(2000)
+        servos.P0.setAngle(150)
     }
 })
